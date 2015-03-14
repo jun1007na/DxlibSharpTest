deb6a7883c7795007ef0da92365659a2f24ca2df
jun1007na <junya.n.107+github@gmail.com>
2015-03-14 11:27:26 +0900

    first commit

---
+++ DxLibSharpTest/CodeFile1.cs
@@ -0,0 +1,33 @@
+﻿using DxLibDLL;
+using System;
+
+namespace Test
+{
+    class TestApplication
+    {
+        ///<summary>
+        ///アプリケーションのエントリーポイント（ここからプログラムが始まる）
+        ///</summary>
+        [STAThread]
+        static void Main()
+        {
+            DX.ChangeWindowMode(DX.TRUE);
+
+            DX.SetGraphMode(800, 600, 32);
+
+            if (DX.DxLib_Init() == -1)
+            {
+                return;
+            }
+
+            DX.DrawBox(100, 100, 200, 150, DX.GetColor(255, 0, 255),DX.TRUE);
+            DX.DrawString(100,200,"This is Sample",DX.GetColor(255,255,64));
+
+            DX.WaitKey();
+
+            DX.DxLib_End();
+
+            return;
+        }
+    }
+}
